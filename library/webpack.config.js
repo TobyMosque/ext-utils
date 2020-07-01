b5753381e3eb0cec36865066a3cdf14ceb009aea
Tobias <Tobias@DESKTOP-CRCGFNT>
2020-07-01 11:36:41 -0300

    commit

--- library/webpack.config.js
+++ library/webpack.config.js
@@ -1,33 +1,38 @@
 const path = require('path')
 const nodeExternals = require('webpack-node-externals')
 
-module.exports = [
-  {
-    target: "web",
-    entry: {
-      app: ["./src/entry.umd.js"]
-    },
-    output: {
-      path: path.resolve(__dirname, "./dist"),
-      filename: "index.umd.js"
-    },
-    resolve: {
-      alias: {
-        'quasar': path.join(__dirname, './umd-alias/quasar.js'),
-        'vue': path.join(__dirname, './umd-alias/vue.js')
-      }
-    }
+const webConfig = {
+  target: "web",
+  entry: {
+    app: ["./src/entry.umd.js"]
   },
-  {
-    target: "node",
-    entry: {
-      app: ["./src/index.js"]
-    },
-    output: {
-      path: path.resolve(__dirname, "./dist"),
-      filename: "index.common.js",
-      libraryTarget: 'commonjs2'
-    },
-    externals: [nodeExternals()]
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index.umd.js"
+  },
+  resolve: {
+    alias: {
+      'quasar': path.join(__dirname, './umd-alias/quasar.js'),
+      'vue': path.join(__dirname, './umd-alias/vue.js')
+    }
   }
-]
+}
+
+const dev = process.env.NODE_ENV === 'development'
+const nodeConfig = {
+  target: "node",
+  mode: dev ? 'development' : 'production',
+  devtool: dev ? '#cheap-module-eval-source-map' : '#source-map',
+  entry: {
+    app: ["./src/index.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: '[name]${fileHash}.js',
+    chunkFilename: '[name]${chunkHash}.js',
+    libraryTarget: 'commonjs2'
+  },
+  externals: [nodeExternals()]
+}
+
+module.exports = [ webConfig, nodeConfig ]
